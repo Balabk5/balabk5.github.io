39ec8e5865ac7251993c3bcf5cdd87929c302a89
TheDevBk <48986470+balarival@users.noreply.github.com>
2024-10-23 23:05:45 +0530

    added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,8 +10,21 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com"  />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
           {/* <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" /> */}
-
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-ETTRE1E7WJ`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ETTRE1E7WJ', {
+              page_path: window.location.pathname,
+            });
+            `,
+          }}
+        />
         </Head>
+        
         <body>
           <Main />
           <NextScript />
